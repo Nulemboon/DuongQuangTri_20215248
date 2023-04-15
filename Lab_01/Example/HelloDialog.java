d97287865daa72d92fa2bc47f667c24992a6fca6
Nulemboon <tri2003714@gmail.com>
2023-04-16 00:58:19 +0700

    upload

--- Lab_01/Example/HelloDialog.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.JOptionPane;
-public class HelloDialog {
-    public static void main (String[] args) {
-        String result;
-        result = JOptionPane.showInputDialog("Please input your name:");
-        JOptionPane.showMessageDialog(null, "Hi " + result + "!");
-    }
-}
