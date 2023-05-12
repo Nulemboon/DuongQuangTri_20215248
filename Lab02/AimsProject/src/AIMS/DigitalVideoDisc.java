95330b9c9bfca736a578957c700dac608d9c1b2e
Nulemboon <tri2003714@gmail.com>
2023-05-12 08:44:03 +0700

    submit passing param

--- Lab02/AimsProject/src/AIMS/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/AIMS/DigitalVideoDisc.java
@@ -11,6 +11,11 @@ public class DigitalVideoDisc {
     public String getTitle() {
         return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getCategory() {
         return category;
     }
